05972a5bdca2a9eca49abb457909171ce56c9110
hbontz <hilarybontz@gmail.com>
2017-07-11 15:00:57 -0700

    m

---
+++ src/BookShelf.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import Shelf from './Shelf'
+
+
+function BookShelf(props) {
+  return (
+    	          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <Shelf 
+                  shelfTitle={"Currently Reading"} 
+                  books={props.bookList.filter(book => book.shelf === "currentlyReading")}
+                  shelfChange={(book, shelf) =>
+                    {props.onShelfMove(book, shelf)}} />
+                <Shelf 
+                  shelfTitle={"Want to Read"} 
+                  books={props.bookList.filter(book => book.shelf === "wantToRead")} 
+                  shelfChange={(book, shelf) =>
+                    {props.onShelfMove(book, shelf)}} />
+                <Shelf 
+                  shelfTitle={"Read"}
+                  books={props.bookList.filter(book => book.shelf === "read")}
+                  shelfChange={(book, shelf) =>
+                    {props.onShelfMove(book, shelf)}} />
+              </div>
+            </div>
+            <div className="open-search"><Link
+            className="open-search"
+            to="/search">
+           Add a book
+            </Link>
+            </div>
+          </div>
+        )
+}
+
+export default BookShelf
---
+++ src/SearchBooks.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Shelf from './Shelf'
+
+class SearchBooks extends Component {
+
+
+  state = {
+    query: '',
+    searchList: []
+  }
+
+  componentWillReceiveProps(nextProps){
+  	this.setState({searchList: nextProps.searchList})
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query })
+    this.props.onSearchTerm(query)
+  }
+
+  render() {
+    const { query } = this.state
+    return (
+    	<div className="search-books">
+            <div className="search-books-bar">
+            	<Link 
+            		className="close-search"
+            		to="/"
+
+            	></Link>
+            	<div className="search-books-input-wrapper">
+            		<input 
+            			type="text" 
+            			placeholder="Enter search term..."
+            			value={query}
+            			onChange={(event) => this.updateQuery(event.target.value)}
+            		/>
+              	</div>
+            </div>
+            <Shelf 
+            	shelfTitle={" "}
+            	books={this.state.searchList ? this.state.searchList : this.props.searchList}
+            	shelfChange={(book, shelf) =>
+                  {this.props.onShelfMove(book, shelf)}} />
+        </div>
+    )
+  }
+}
+
+export default SearchBooks
