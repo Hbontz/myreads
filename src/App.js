6213d9d5f266a3a8c5801d79dd38f46cf6b32db8
hbontz <hilarybontz@gmail.com>
2017-07-11 15:22:23 -0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import SearchBooks from './SearchBooks'
-import BookShelf from './BookShelf'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from "react";
+import { Route } from "react-router-dom";
+import SearchBooks from "./SearchBooks";
+import BookShelf from "./BookShelf";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
 
 class BooksApp extends React.Component {
   state = {
     bookList: [],
-    searchList:[]
-  }
+    searchList: []
+  };
   componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ bookList: books })
-    })
+    BooksAPI.getAll().then(books => {
+      this.setState({ bookList: books });
+    });
   }
   bookSearch(searchTerm) {
-    if (searchTerm){
-    BooksAPI.search(searchTerm).then((books) => {
-      if (books && books.length) {
-        this.updateShelfValue(books)
-        this.setState({ searchList: books })
-      }
-    })
-  } else {
-    this.setState({searchList: []})
-  }
+    if (searchTerm) {
+      BooksAPI.search(searchTerm).then(books => {
+        if (books && books.length) {
+          this.updateShelfValue(books);
+          this.setState({ searchList: books });
+        }
+      });
+    } else {
+      this.setState({ searchList: [] });
+    }
   }
   shelfMove(book, shelf) {
-    BooksAPI.update(book, shelf)
-    BooksAPI.getAll().then((books) => {
-      this.setState({ bookList: books })
-      })
+    BooksAPI.update(book, shelf);
+    BooksAPI.getAll().then(books => {
+      this.setState({ bookList: books });
+    });
   }
-  updateShelfValue(booksArr){
-    for(let i = 0; i < booksArr.length; i++) {
-      for (let j = 0; j < this.state.bookList.length; j++){
-        if (booksArr[i].id === this.state.bookList[j].id){
-          booksArr[i].shelf = this.state.bookList[j].shelf
+  updateShelfValue(booksArr) {
+    for (let i = 0; i < booksArr.length; i++) {
+      for (let j = 0; j < this.state.bookList.length; j++) {
+        if (booksArr[i].id === this.state.bookList[j].id) {
+          booksArr[i].shelf = this.state.bookList[j].shelf;
         }
       }
     }
@@ -46,26 +46,33 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path='/' render={() => (
-          <BookShelf
-            bookList={this.state.bookList}
-            onShelfMove={(book, shelf) => {
-              this.shelfMove(book, shelf)}}
-          />
-        )}/>
-        <Route path='/search' render={() => (
-          <SearchBooks
-            onSearchTerm={searchTerm => {
-              this.bookSearch(searchTerm)}}
-            searchList={this.state.searchList}
-            onShelfMove={(book, shelf) => {
-              this.shelfMove(book, shelf)
+        <Route
+          exact
+          path="/"
+          render={() =>
+            <BookShelf
+              bookList={this.state.bookList}
+              onShelfMove={(book, shelf) => {
+                this.shelfMove(book, shelf);
               }}
-          />
-        )}/>
+            />}
+        />
+        <Route
+          path="/search"
+          render={() =>
+            <SearchBooks
+              onSearchTerm={searchTerm => {
+                this.bookSearch(searchTerm);
+              }}
+              searchList={this.state.searchList}
+              onShelfMove={(book, shelf) => {
+                this.shelfMove(book, shelf);
+              }}
+            />}
+        />
       </div>
-    )
-  }  
+    );
+  }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,58 +1,95 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Shelf extends Component {
 	static propTypes = {
-    	books: PropTypes.array.isRequired,
-    	shelfChange: PropTypes.func.isRequired,
-    	shelfTitle: PropTypes.string
-  }
+		books: PropTypes.array.isRequired,
+		shelfChange: PropTypes.func.isRequired,
+		shelfTitle: PropTypes.string
+	};
 	state = {
 		shelfValue: ""
-	}
+	};
 
 	valueChange(valString) {
-		this.setState({shelfValue: valString})
+		this.setState({ shelfValue: valString });
 	}
 
 	render() {
-
-		const { books } = this.props
+		const { books } = this.props;
 
 		return (
 			<div className="bookshelf">
-                  <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid"> { 
-                    	books.map((book, i) =>
-                      <li key={book.id}>
-				<div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks !== undefined ? book.imageLinks.thumbnail :''})`}}></div>
-                        <div className="book-shelf-changer">    
-                            <select 
-                            	value={this.state.shelfValue || book.shelf} 
-                            	onChange={(event) => {
-                            		this.props.shelfChange(book, event.target.value)
-                            		this.valueChange(event.target.value)
-                            	}}>
-                            	<option value="none" disabled>Current Shelf...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                	<div className="book-title">{book.title}</div>
-                	<div className="book-authors">{book.authors ? book.authors.join(", ") : ""}</div>
-                </div>
-            </li>)}
-                    </ol>
-                  </div>
-                </div>
-        )
-    }                 
+				<h2 className="bookshelf-title">
+					{this.props.shelfTitle}
+				</h2>
+				<div className="bookshelf-books">
+					<ol className="books-grid">
+						{" "}{books.map((book, i) =>
+							<li key={book.id}>
+								<div className="book">
+									<div className="book-top">
+										<div
+											className="book-cover"
+											style={{
+												width: 128,
+												height: 193,
+												backgroundImage: `url(${book.imageLinks !==
+												undefined
+													? book.imageLinks.thumbnail
+													: ""})`
+											}}
+										/>
+										<div className="book-shelf-changer">
+											<select
+												value={
+													this.state.shelfValue ||
+													book.shelf
+												}
+												onChange={event => {
+													this.props.shelfChange(
+														book,
+														event.target.value
+													);
+													this.valueChange(
+														event.target.value
+													);
+												}}
+											>
+												<option value="none" disabled>
+													Current Shelf...
+												</option>
+												<option value="currentlyReading">
+													Currently Reading
+												</option>
+												<option value="wantToRead">
+													Want to Read
+												</option>
+												<option value="read">
+													Read
+												</option>
+												<option value="none">
+													None
+												</option>
+											</select>
+										</div>
+									</div>
+									<div className="book-title">
+										{book.title}
+									</div>
+									<div className="book-authors">
+										{book.authors
+											? book.authors.join(", ")
+											: ""}
+									</div>
+								</div>
+							</li>
+						)}
+					</ol>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Shelf
+export default Shelf;
